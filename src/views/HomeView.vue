a6c992f7d9e4c4bcbde00b15119550ea6e82cfb0
Jaroslav Kalistsuk <jaroslav@byteflip.de>
2024-03-03 20:38:45 +0100

    Minor tweaks

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,15 @@
 <template>
   <main class="px-4 container mx-auto max-w-xl">
-    <h1 class="mt-6 text-5xl text-center">Math Park</h1>
+    <h1 class="mt-6 text-5xl text-center flex items-center justify-center">
+      <SvgIcon :path="mdiCalculatorVariantOutline" :size="42" />
+      Math Park
+    </h1>
     <QuizContainer />
   </main>
 </template>
 
 <script setup lang="ts">
 import QuizContainer from '@/components/QuizContainer.vue'
+import { mdiCalculatorVariantOutline } from '@mdi/js';
+import SvgIcon from '@/components/SvgIcon.vue';
 </script>
