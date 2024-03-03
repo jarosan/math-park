f7111a6132ded8d4a66a382a9fc58b5591c3816c
Jaroslav Kalistsuk <jaroslav@byteflip.de>
2024-03-03 13:00:44 +0100

    postcss/tailwind/cleanup

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{html,js,ts,vue}'],
+  // theme: {
+  //   extend: {},
+  // },
+  plugins: [],
+}
